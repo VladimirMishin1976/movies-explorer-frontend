4f495a974c826d15a899ff292e081dddcd23c117
Vladimir Mishin <drli396@yandex.ru>
2022-01-30 21:12:24 +0300

    fix: MAIN_URL_BACKEND

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const MAIN_URL_BACKEND = 'https://api.movies.mishin.nomoredomains.club';
-export const MAIN_URL_BACKEND = 'http://localhost:3001';
+export const MAIN_URL_BACKEND = 'https://api.movies.mishin.nomoredomains.club';
+// export const MAIN_URL_BACKEND = 'http://localhost:3001';
 
 export const BEATFILMS_URL_BACKEND_MOVIES = 'https://api.nomoreparties.co/beatfilm-movies';
 export const BEATFILMS_URL_BACKEND = 'https://api.nomoreparties.co';
